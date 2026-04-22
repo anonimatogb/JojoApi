05a35fee0fb47200951008bec8ae3eb8d6416c1c
anonimatogb <gabrielcavalcante22@outlook.com>
2026-04-22 07:47:05 -0300

    tudp

--- index.php
+++ index.php
@@ -4,24 +4,29 @@ $url="jojos.json";
 $response=file_get_contents($url);
 $jojos=json_decode($response,true);
 
+echo "<h1>Jojo's Bizarre Adventure</h1>";
+echo "<link rel='stylesheet' href='style.css'>";
 
+echo"<div class='container'>";
 foreach($jojos['stardust'] as $jojo){
-    
-    echo "<h2>".$jojo["stand"]."</h2>";
-    echo "<h3>".$jojo["type"][0]."</h3>";
-    echo "<h3>".$jojo["type"][1]."</h3>";
-    echo "<h3>".$jojo["type"][2]."</h3>";
-    echo "<img src='" . $jojo['stand_images'][0] . "'>";
-    echo "<h3>".$jojo["user"]."</h3>";
-    echo "<h3>".$jojo["gender"]."</h3>";
-    echo "<h3>".$jojo["hair_color"]."</h3>";
-    echo "<h3>".$jojo["eye_color"]."</h3>";
-    echo "<img src='" . $jojo['user_images'][0] . "'>";
+ echo "<div class='jojo'>";
 
+echo "<img src='" . $jojo['stand_images'][0] . "'>";
 
+echo "<div class='jojo-content'>";
+echo "<h2>".$jojo["stand"]."</h2>";
+
+echo "<h3><strong>Usuário:</strong> ".$jojo["user"]."</h3>";
+echo "<h3>Gênero: ".$jojo["gender"]."</h3>";
+echo "<h3>Cabelo: ".$jojo["hair_color"]."</h3>";
+echo "<h3>Olhos: ".$jojo["eye_color"]."</h3>";
+
+echo "</div>";
+
+echo "</div>";
 }
 
-
+echo"</div>";
 
 
 ?>
